64a001645441b528e3a23e0022467b603c0ba432
MishiMads <mnj-777@hotmail.com>
2024-11-15 13:25:16 +0100

    Bælterotation er okay-ish nu

--- Assets/FFOS-VR-Tutorial-Resources-main/FFOS-VR-Tutorial-Resources-main/FFOSBodySocketInventory/BodySocketInventory.cs
+++ Assets/FFOS-VR-Tutorial-Resources-main/FFOS-VR-Tutorial-Resources-main/FFOSBodySocketInventory/BodySocketInventory.cs
@@ -11,37 +11,68 @@ public class bodySocket
 }
 
 public class BodySocketInventory : MonoBehaviour
-
 {
     public GameObject HMD;
     public bodySocket[] bodySockets;
+    public float offset = 5f;
+    public float rotationLagFactor = 0.1f; // Lower values mean slower rotation lag
+    public float rotationLimit = 45f; // Maximum rotation deviation in degrees
 
     private Vector3 _currentHMDlocalPosition;
     private Quaternion _currentHMDRotation;
+    private Quaternion _targetBeltRotation;
+
     void Update()
     {
         _currentHMDlocalPosition = HMD.transform.localPosition;
         _currentHMDRotation = HMD.transform.rotation;
-        foreach(var bodySocket in bodySockets)
+
+        // Update height of each body socket
+        foreach (var bodySocket in bodySockets)
         {
             UpdateBodySocketHeight(bodySocket);
         }
+
         UpdateSocketInventory();
     }
 
     private void UpdateBodySocketHeight(bodySocket bodySocket)
     {
-
-        bodySocket.gameObject.transform.localPosition = new Vector3(bodySocket.gameObject.transform.localPosition.x,(_currentHMDlocalPosition.y * bodySocket.heightRatio), bodySocket.gameObject.transform.localPosition.z);
+        bodySocket.gameObject.transform.localPosition = new Vector3(
+            bodySocket.gameObject.transform.localPosition.x,
+            (_currentHMDlocalPosition.y * bodySocket.heightRatio),
+            bodySocket.gameObject.transform.localPosition.z
+        );
     }
 
     private void UpdateSocketInventory()
     {
-        transform.localPosition = new Vector3(_currentHMDlocalPosition.x, 0, _currentHMDlocalPosition.z);
-        transform.rotation = new Quaternion(transform.rotation.x, _currentHMDRotation.y, transform.rotation.z, _currentHMDRotation.w);
-        /*
-        transform.localPosition = new Vector3(_currentHMDlocalPosition.x, 0, _currentHMDlocalPosition.z);
-        transform.rotation = new Quaternion(transform.rotation.x, 0, transform.rotation.z, _currentHMDRotation.w);
-        */
+        // Update the toolbelt's position to match the HMD's position on the XZ plane
+        transform.localPosition = new Vector3(
+            _currentHMDlocalPosition.x,
+            0, // Keep the belt at a fixed height
+            _currentHMDlocalPosition.z
+        );
+
+        // Calculate the desired belt rotation with an offset
+        float targetYRotation = _currentHMDRotation.eulerAngles.y + offset;
+
+        // Clamp the rotation difference to the specified limit
+        float currentYRotation = transform.rotation.eulerAngles.y;
+        float rotationDifference = Mathf.DeltaAngle(currentYRotation, targetYRotation);
+
+        if (Mathf.Abs(rotationDifference) > rotationLimit)
+        {
+            targetYRotation = currentYRotation + Mathf.Sign(rotationDifference) * rotationLimit;
+        }
+
+        // Interpolate towards the clamped target rotation
+        _targetBeltRotation = Quaternion.Euler(0, targetYRotation, 0);
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            _targetBeltRotation,
+            rotationLagFactor * Time.deltaTime
+        );
+        
     }
 }
