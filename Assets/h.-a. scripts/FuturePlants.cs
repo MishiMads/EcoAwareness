a9504c78fe1c7fc17b25ed48179e23d002dd6e61
mhumme20 <mhumme20@student.aau.dk>
2024-11-20 10:50:18 +0100

    changes before merge

--- Assets/h.-a. scripts/FuturePlants.cs
+++ Assets/h.-a. scripts/FuturePlants.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,6 +10,10 @@ public class FuturePlants : MonoBehaviour
   public GameObject BFPlantPrefab;
   public GameObject HedgeHogPlantPrefab;
   
+  
+  public GameObject BeeFuture;
+  public GameObject BFFuture;
+  public GameObject HedgeHogFuture;
   private void OnEnable()
   {
     // Subscribe to the SceneLoaded event
@@ -28,16 +33,20 @@ public class FuturePlants : MonoBehaviour
     if (QuestManager.Instance.beeQuestComplete)
     {
       Instantiate(BeePlantPrefab , QuestManager.Instance.BeePlant, Quaternion.identity);
+      BeeFuture.gameObject.SetActive(true);
     }
     
     if (QuestManager.Instance.BFQuestComplete)
     {
       Instantiate(BFPlantPrefab , QuestManager.Instance.BFPlant, Quaternion.identity);
+      BFFuture.gameObject.SetActive(true);
     }
     
     if (QuestManager.Instance.HedgeHogQuestComplete)
     {
       Instantiate(HedgeHogPlantPrefab , QuestManager.Instance.HedgeHogPlant, Quaternion.identity);
+      HedgeHogFuture.gameObject.SetActive(true);
+      
     }
   }
 }
