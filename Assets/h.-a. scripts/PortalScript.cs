b22833c0e45fd39d58d676251fd31266371ea21a
mhumme20 <mhumme20@student.aau.dk>
2024-11-22 14:38:33 +0100

    Portal script
    feel free to use the portal script

---
+++ Assets/h.-a. scripts/PortalScript.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalScript : MonoBehaviour
+{
+ private int EndSceneIndex = 3;
+
+
+ private void OnTriggerEnter(Collider other)
+ {
+  if (other.gameObject.CompareTag("Player"))
+  {
+   SceneManager.LoadScene(sceneBuildIndex: EndSceneIndex);
+  }
+ }
+}
