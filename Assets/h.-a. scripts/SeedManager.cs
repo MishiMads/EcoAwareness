78762380042270beee566621b5384d44cdfd1cf8
mhumme20 <mhumme20@student.aau.dk>
2024-11-22 14:24:10 +0100

    Seed Manager
    This script makes seeds disapear upon the scene being loaded, if the player has completed the associated quests.

---
+++ Assets/h.-a. scripts/SeedManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SeedManager : MonoBehaviour
+{
+
+    public GameObject BeeSeed;
+    public GameObject ButteflySeed;
+    public GameObject HedgeHogSeed;
+    private void OnEnable()
+    {
+        // Subscribe to the SceneLoaded event
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+
+
+    private void OnDisable()
+    {
+        // Unsubscribe to prevent memory leaks
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (QuestManager.Instance.beeQuestComplete)
+        {
+            BeeSeed.gameObject.SetActive(false);
+        }
+
+        if (QuestManager.Instance.BFQuestComplete)
+        {
+            ButteflySeed.gameObject.SetActive(false);
+        }
+
+        if (QuestManager.Instance.HedgeHogQuestComplete)
+        {
+            HedgeHogSeed.gameObject.SetActive(false);
+        }    
+        
+    }
+
+
+
+}
